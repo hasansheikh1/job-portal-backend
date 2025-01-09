6728ebb7beee82e45a620141d2e73180bb7409db
Hasan Faysal <hasanfaysal17@gmail.com>
2025-01-09 18:21:28 +0500

    changes

--- controller/jobCtrl.js
+++ controller/jobCtrl.js
@@ -5,12 +5,30 @@ const asyncHandler = require('express-async-handler')
 
 const createJob = asyncHandler(async (req, res) => {
 
-    const { email } = req?.body
 
-    res.json({
-        email: email
-    })
-    
+
+    // if(req.user.role!='employer'){
+    //     return res.status(403).json({message:"Access denied. Only employers can create jobs."})
+    // }
+    try {
+        const { body } = req.body
+        console.log("req ", body)
+        const job = await Job.create({
+            ...req.body,
+            employerId: req?.user._id,
+        })
+
+        res.status(201).json({
+
+            message: "Job created successfully",
+
+        })
+    } catch (error) {
+        console.log("Error creating job ", error)
+
+    }
+
+
 
 
 })
--- models/jobModel.js
+++ models/jobModel.js
@@ -5,32 +5,38 @@ const crypto = require("crypto");
 // Declare the Schema of the Mongo model
 var jobSchema = new mongoose.Schema({
 
-    eemployerId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
+    employerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employer',
+        // required: true
     },
 
     jobTitle: {
         type: String,
         required: true,
         index: true,
+        minlength: 3,
+        maxlength: 100,
     },
 
-  
+
     salary: {
         type: Number,
         // required: true,
+        min: 0,
     },
+
     location: {
         type: String,
         required: true
     },
     jobDesc: {
         type: String,
+        maxlength: 1000,
     },
-    status:{
+    status: {
         type: String,
-        enum: ['user', 'admin', 'moderator']
+        enum: ['open', 'closed']
     }
 
 
